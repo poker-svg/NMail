bf2e06fb6263cbd825c2f76ab86ea124e9cb71d1
Xin <201220028@smail.nju.edu.cn>
2023-03-29 16:52:35 +0800

    1.写了点报告 2.完善了退出和登录功能

--- src/assets/js/ajaxPre.js
+++ src/assets/js/ajaxPre.js
@@ -2,7 +2,7 @@
  * @Author: Xin 201220028@smail.nju.edu.cn
  * @Date: 2023-03-26 19:18:44
  * @LastEditors: Xin 201220028@smail.nju.edu.cn
- * @LastEditTime: 2023-03-28 12:48:11
+ * @LastEditTime: 2023-03-29 16:02:27
  * @FilePath: \NMail\src\assets\js\ajaxPre.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,7 @@
 // $.ajaxPrefilter的options 参数时 $.ajax或$.post或$.get的对象
 $.ajaxPrefilter(function (options) {
   if (options.url.startsWith("/my")) {
+    // 自动配置headers中的用于身份认证的Authorization
     options.headers = { Authorization: localStorage.getItem("token") || "" };
 
     options.complete = function (res) {
@@ -18,7 +19,7 @@ $.ajaxPrefilter(function (options) {
         res.responseJSON.msg === "身份认证失败"
       ) {
         localStorage.removeItem("token");
-        location.href = "/login.html";
+        location.href = "/src/login.html";
       }
     };
   }
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,50 +1,63 @@
-$(function() {
-    getUserInfo()
+/*
+ * @Author: Xin 201220028@smail.nju.edu.cn
+ * @Date: 2023-03-26 19:18:44
+ * @LastEditors: Xin 201220028@smail.nju.edu.cn
+ * @LastEditTime: 2023-03-29 15:47:46
+ * @FilePath: \NMail\src\assets\js\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+$(function () {
+  getUserInfo();
 
-    $('#btnLogout').on('click', function() {
-        layui.layer.confirm('确定要退出吗？', {
-            icon: 3,
-            title: '提示',
-            btn: ['确定', '取消']
-        }, function(index) {
-            localStorage.removeItem('token')
-            location.href = '/login.html'
-            layui.layer.close(index)
-        });
-    })
-})
+  $("#btnLogout").on("click", function () {
+    layui.layer.confirm(
+      "确定要退出吗？",
+      {
+        icon: 3,
+        title: "提示",
+        btn: ["确定", "取消"],
+      },
+      function (index) {
+        localStorage.removeItem("token");
+        location.href = "/src/login.html";
+        layui.layer.close(index);
+      }
+    );
+  });
+});
 
 function getUserInfo() {
-    $('.layui-nav-img').hide()
-    $.ajax({
-        url: '/my/userinfo',
-        method: 'GET',
-        success: function(res) {
-            if (res.status !== 0) {
-                return layui.layer.msg(res.msg)
-            }
-            renderAvatar(res.data)
-        }
-    })
+  $(".layui-nav-img").hide();
+  $.ajax({
+    url: "/my/userinfo",
+    method: "GET",
+    success: function (res) {
+      //   console.log(res);
+      if (res.status !== 0) {
+        return layui.layer.msg(res.msg);
+      }
+      renderAvatar(res.data);
+    },
+  });
 }
 
 function renderAvatar(user) {
-    // 欢迎用户
-    var name = user.nickname || user.username
-    $('#welcome').html(`欢迎 ${name}`)
+  // 欢迎用户
+  var name = user.nickname || user.username;
+  $("#welcome").html(`欢迎 ${name}`);
 
-    // 用户头像渲染
-    if (user.user_pic) {
-        $('.layui-nav-img').attr('src', user.user_pic).show()
-        $('.text-avatar').hide()
-    } else {
-        let first = name[0].toUpperCase()
-        $('.text-avatar').html(first).show()
-        $('.layui-nav-img').hide()
-    }
+  // 用户头像渲染
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    let first = name[0].toUpperCase();
+    $(".text-avatar").html(first).show();
+    $(".layui-nav-img").hide();
+  }
 }
 
 function setNavSelected(origin, current) {
-    $(origin).addClass('layui-this')
-    $(current).removeClass('layui-this')
+  $(origin).addClass("layui-this");
+  $(current).removeClass("layui-this");
 }
