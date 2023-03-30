fa8742f96f4e51f3aa23e7e79aec879ffc8088d5
Xin <201220028@smail.nju.edu.cn>
2023-03-30 13:38:52 +0800

    完善打招呼功能

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -2,7 +2,7 @@
  * @Author: Xin 201220028@smail.nju.edu.cn
  * @Date: 2023-03-26 19:18:44
  * @LastEditors: Xin 201220028@smail.nju.edu.cn
- * @LastEditTime: 2023-03-29 17:00:12
+ * @LastEditTime: 2023-03-30 13:38:23
  * @FilePath: \NMail\src\assets\js\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -43,8 +43,29 @@ function getUserInfo() {
 
 function renderAvatar(user) {
   // 欢迎用户
-  var name = user.nickname || user.username;
-  $("#welcome").html(`欢迎 ${name}`);
+  let name = user.nickname || user.username;
+  let now = new Date();
+  let hour = now.getHours();
+  let hello = "早上好";
+  if (hour < 6) {
+    hello = "凌晨好";
+  } else if (hour < 9) {
+    hello = "早上好";
+  } else if (hour < 12) {
+    hello = "上午好";
+  } else if (hour < 14) {
+    hello = "中午好";
+  } else if (hour < 17) {
+    hello = "下午好";
+  } else if (hour < 19) {
+    hello = "傍晚好";
+  } else if (hour < 22) {
+    hello = "晚上好";
+  } else {
+    hello = "夜里好";
+  }
+
+  $("#welcome").html(`${hello} ${name}`);
 
   // 用户头像渲染
   if (user.user_pic !== null) {
