c86f4a96ba1567f7ff6a3a80a81d60c04acf2d46
Xin <201220028@smail.nju.edu.cn>
2023-04-03 10:40:02 +0800

    完善前端收件箱的分页操作

--- front/assets/js/mails/readMails.js
+++ front/assets/js/mails/readMails.js
@@ -2,7 +2,7 @@
  * @Author: Xin 201220028@smail.nju.edu.cn
  * @Date: 2023-03-30 13:49:37
  * @LastEditors: Xin 201220028@smail.nju.edu.cn
- * @LastEditTime: 2023-04-03 09:17:59
+ * @LastEditTime: 2023-04-03 10:35:53
  * @FilePath: \NMail\front\assets\js\mails\readMails.js
  * @Description: 前端读取收件箱操作
  */
@@ -10,7 +10,7 @@ $(function () {
   let default_query = {
     // 默认读取的页面参数
     pagenum: 1, // 页码数
-    pagesize: 5, // 每页显示的信件条数
+    pagesize: 2, // 每页显示的信件条数
   };
 
   initReceiveMailsList();
@@ -31,7 +31,34 @@ $(function () {
         let htmlStr = template("tpl-table", res);
         $("tbody").html(htmlStr);
 
-        // renderPage(res.total);
+        renderPage(res.total);
+      },
+    });
+  }
+
+  // 渲染分页
+  function renderPage(total) {
+    layui.laypage.render({
+      elem: "pageBox",
+      count: total,
+      limit: default_query.pagesize,
+      curr: default_query.pagenum,
+      layout: ["count", "limit", "prev", "page", "next", "skip"],
+      limits: [2, 3, 5, 10],
+      jump: function (obj, first) {
+        default_query.pagenum = obj.curr;
+        default_query.pagesize = obj.limit;
+
+        /* 注意: jump回调有两种触发方式
+                1、当点击分页的页码时
+                2、当layui.laypage.render方法被调用时
+                如果是2方式触发，则可能出现死循环，initArticleList -> renderPage -> jump -> initArticleLis
+                所以需要判断jump是否是2触发的，排除该种方式触发产生的initArticleList调用
+                layui的jump回调函数的第二个参数first就是来提示jump的触发方式，如果first为true，则为2触发，否则为1触发
+            */
+        if (!first) {
+          initReceiveMailsList();
+        }
       },
     });
   }
